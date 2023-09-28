3f996c3d9af24c1e6992df1e78175a042bb575b1
lucasito028 <lucastoshihiroito28@gmail.com>
2023-09-28 12:55:29 -0300

    pao

--- Polimorfismo de interfaces/Calculadora.php
+++ Polimorfismo de interfaces/Calculadora.php
@@ -6,9 +6,31 @@
         protected $n1;
         protected $n2;
 
+        
         function __construct($n1, $n2){
             $this->$n1 = $n1;
             $this->$n2 = $n2;
+        }
+
+
+        function adicao(){
+            $this->resultado = $this->n1 + $this->n2;
+            return $this->resultado;
+        }
+
+        function subtracao(){
+            $this->resultado = $this->n1 - $this->n2;
+            return $this->resultado;
+        }
+    
+        function multiplicacao(){
+            $this->resultado = $this->n1 * $this->n2;
+            return $this->resultado;
+        }
+    
+        function divisao(){
+            $this->resultado = $this->n1 / $this->n2;
+            return $this->resultado;
         }
 
     }
